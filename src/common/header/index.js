9cfd13f8ecdb15ab188b53b2de36706887275de4
Yejj <1271361907@qq.com>
2019-04-08 22:38:53 +0800

    first

---
+++ src/common/header/index.js
@@ -0,0 +1,35 @@
+import React,{ Component } from 'react';
+import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button,SearchWrapper} from './style';
+class Header extends Component {
+    
+    render(){
+        return(
+
+            <HeaderWrapper>
+              <Logo href='/' /> 
+              <Nav>
+                  <NavItem className='left active'>首页</NavItem>
+                  <NavItem className='left'>下载APP</NavItem>
+                  <NavItem className='right'>登录</NavItem>
+                  <NavItem className='right'>
+                    <i className="iconfont">&#xe636;</i>
+                  </NavItem>
+                  <SearchWrapper>
+                    <NavSearch></NavSearch>
+                    <i className="iconfont">&#xe614;</i>
+                  </SearchWrapper>
+                  
+              </Nav>
+              <Addition>
+                  <Button className='reg'>注册</Button>
+                  <Button className='writting'>
+                  <i className="iconfont">&#xe615;</i>
+                  写文章</Button>
+              </Addition>
+            </HeaderWrapper>
+        
+        )
+    }
+}
+
+export default Header;
