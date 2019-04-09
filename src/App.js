9b51ec00341f2c73ff4c1013e43da893d09ee018
Yejj <1271361907@qq.com>
2019-04-09 23:06:49 +0800

    header finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component,Fragment } from 'react';
+import { Provider} from 'react-redux';
 import Header from './common/header';
 import {GlobalStyle}  from './style.js';
 import {IconStyle} from './static/iconfont/iconfont' ;
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <IconStyle/>
-        <GlobalStyle/>
-        <Header/>
-      </Fragment>
+     
+        <Provider store = {store}>
+          <Fragment>
+            <IconStyle/>
+            <GlobalStyle/>
+            <Header/>
+          </Fragment>
+        </Provider>
+     
 
     );
   }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,35 +1,137 @@
-import React,{ Component } from 'react';
-import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button,SearchWrapper} from './style';
-class Header extends Component {
-    
-    render(){
-        return(
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
+import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,SearchInfo,SearchInfoTitle,SearchInfoSwitch,SearchInfoList,SearchInfoItem,Addition,Button,SearchWrapper} from './style';
 
-            <HeaderWrapper>
-              <Logo href='/' /> 
-              <Nav>
-                  <NavItem className='left active'>首页</NavItem>
-                  <NavItem className='left'>下载APP</NavItem>
-                  <NavItem className='right'>登录</NavItem>
-                  <NavItem className='right'>
-                    <i className="iconfont">&#xe636;</i>
-                  </NavItem>
-                  <SearchWrapper>
-                    <NavSearch></NavSearch>
-                    <i className="iconfont">&#xe614;</i>
-                  </SearchWrapper>
-                  
-              </Nav>
-              <Addition>
-                  <Button className='reg'>注册</Button>
-                  <Button className='writting'>
-                  <i className="iconfont">&#xe615;</i>
-                  写文章</Button>
-              </Addition>
-            </HeaderWrapper>
-        
+
+
+class Header extends Component{
+ getListArea = ()=>{
+    
+    const {focused,list,page,totalPage, mouseIn,handlerMouseEnter,handlerMouseLeave,handleChangePage} = this.props; 
+    const newList = list.toJS();
+    const pageList = [];
+    if(newList.length){
+      for( let i=(page-1)*10;i<page*10;i++){
+        pageList.push(
+         <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
         )
+   }
     }
+   
+    
+    if(focused || mouseIn){
+        return(
+          <SearchInfo onMouseEnter={handlerMouseEnter}
+          onMouseLeave={handlerMouseLeave}>
+          <SearchInfoTitle>
+             热门搜索
+             <SearchInfoSwitch onClick={()=>handleChangePage(page,totalPage,this.spinIcon)}
+             >
+             <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
+             换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <div>
+            <SearchInfoList>
+               {pageList}
+            </SearchInfoList> 
+          </div>
+        </SearchInfo>
+        )
+    }else{
+      return null;
+    }
+  }
+
+    render(){
+
+      const {focused,handleInputFocus,handleInputBlur,list} =this.props
+      return (
+        <HeaderWrapper>
+                      <Logo href='/' /> 
+                      <Nav>
+                          <NavItem className='left active'>首页</NavItem>
+                          <NavItem className='left'>下载APP</NavItem>
+                          <NavItem className='right'>登录</NavItem>
+                          <NavItem className='right'>
+                            <i className="iconfont">&#xe636;</i>
+                          </NavItem>
+                          <SearchWrapper>
+                            <CSSTransition
+                            in={focused}
+                            timeout={200}
+                            classNames="slide">
+                            <NavSearch
+                             className={focused?'focused':''}
+                             onFocus={()=>handleInputFocus(list)}
+                             onBlur={handleInputBlur}>
+                            </NavSearch>
+                            </CSSTransition>
+                            <i className={focused?'focused iconfont zoom':'iconfont zoom'}>&#xe614;</i>
+                          {this.getListArea()}
+                          </SearchWrapper>
+                          
+                      </Nav>
+                      <Addition>
+                          <Button className='reg'>注册</Button>
+                          <Button className='writting'>
+                          <i className="iconfont">&#xe615;</i>
+                          写文章</Button>
+                      </Addition>
+                    </HeaderWrapper>
+          );
+    } 
+  
 }
 
-export default Header;
+
+
+
+const mapStateToProps = (state) =>{
+  return {
+    focused:  state.getIn(['header', 'focused']),
+    list : state.getIn(['header','list']),
+    page : state.getIn(['header','page']),
+    totalPage : state.getIn(['header','totalPage']),
+    mouseIn:state.getIn(['header','mouseIn'])
+  }
+}
+const mapDispathToProps = (dispatch) => {
+  return {
+    handleInputFocus(list){
+      if(list.size===0){
+        dispatch(actionCreators.getList());
+      }
+    
+      dispatch(actionCreators.searchFocus());
+   },
+   handleInputBlur(){
+  
+      dispatch(actionCreators.searchBlur()); 
+   },
+   handlerMouseEnter(){
+     dispatch(actionCreators.mouseEnter());   
+   },
+   handlerMouseLeave(){
+     dispatch(actionCreators.mouserLeave());  
+   },
+   handleChangePage(page,totalPage,spin){
+    let originAngle = spin.style.transform.replace(/[^0-9]/ig, '');
+    if (originAngle) {
+      originAngle = parseInt(originAngle, 10);
+    }else {
+      originAngle = 0;
+    }
+    spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)';
+     if(page<totalPage){
+      dispatch(actionCreators.changePage(page+1));
+     }else{
+      dispatch(actionCreators.changePage(1));
+     }
+     
+   } 
+  }
+}
+
+export default connect(mapStateToProps,mapDispathToProps)( Header);
